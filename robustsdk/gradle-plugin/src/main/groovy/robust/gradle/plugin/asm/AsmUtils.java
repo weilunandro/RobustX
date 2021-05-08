276116c24f9979b6722674fb737e4c14d1db24b7
weikaiyun <weikaiyun@pwrd.com>
2021-05-08 15:23:13 +0800

    first commit

---
+++ robustsdk/gradle-plugin/src/main/groovy/robust/gradle/plugin/asm/AsmUtils.java
@@ -0,0 +1,9 @@
+package robust.gradle.plugin.asm;
+
+public class AsmUtils {
+    private AsmUtils() {
+    }
+
+    public static final String CONSTRUCTOR = "<init>";
+    public static final String CLASS_INITIALIZER = "<clinit>";
+}
